767d80f7c2be29944488d44a412d0fd8533e9744
ALISHA RAJ <45446699+alisharaj1@users.noreply.github.com>
2020-05-10 00:48:49 +0530

    Add files via upload

---
+++ Flight_Management_System/src/main/java/com/capgemini/fms/entity/Schedule.java
@@ -0,0 +1,85 @@
+package com.capgemini.fms.entity;
+import java.time.LocalDateTime;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name ="schedule")
+@DynamicUpdate(true)
+@DynamicInsert(true)
+
+public class Schedule {
+	@Id
+	private int schedule_id;
+	@ManyToOne
+    @JoinColumn(name="airport_code",nullable=false)
+    private Airport sourceAirport;
+	@ManyToOne
+    @JoinColumn(name="airport_name",nullable=false)
+    private Airport destinationAirport;
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	   private LocalDateTime arrivalTime;
+	 @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	   private LocalDateTime departureTime;
+	public int getSchedule_id() {
+		return schedule_id;
+	}
+	public void setSchedule_id(int schedule_id) {
+		this.schedule_id = schedule_id;
+	}
+	public Airport getSourceAirport() {
+		return sourceAirport;
+	}
+	public void setSourceAirport(Airport sourceAirport) {
+		this.sourceAirport = sourceAirport;
+	}
+	public Airport getDestinationAirport() {
+		return destinationAirport;
+	}
+	public void setDestinationAirport(Airport destinationAirport) {
+		this.destinationAirport = destinationAirport;
+	}
+	public LocalDateTime getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setArrivalTime(LocalDateTime arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+	public LocalDateTime getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(LocalDateTime departureTime) {
+		this.departureTime = departureTime;
+	}
+	public Schedule(int schedule_id, Airport sourceAirport, Airport destinationAirport, LocalDateTime arrivalTime,
+			LocalDateTime departureTime) {
+		super();
+		this.schedule_id = schedule_id;
+		this.sourceAirport = sourceAirport;
+		this.destinationAirport = destinationAirport;
+		this.arrivalTime = arrivalTime;
+		this.departureTime = departureTime;
+	}
+	public Schedule() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Schedule [schedule_id=" + schedule_id + ", sourceAirport=" + sourceAirport + ", destinationAirport="
+				+ destinationAirport + ", arrivalTime=" + arrivalTime + ", departureTime=" + departureTime + "]";
+	}
+
+		
+}
---
+++ Flight_Management_System/src/main/java/com/capgemini/fms/exception/AirportExcept‭ion.java
@@ -0,0 +1,33 @@
+package com.capgemini.fms.exception;
+
+public class AirportExcept‭ion extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AirportExcept‭ion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AirportExcept‭ion(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AirportExcept‭ion(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AirportExcept‭ion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AirportExcept‭ion(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
